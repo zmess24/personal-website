25e3e241fb312146ec2e7ef624e26cfc96ecc0ec
Zac Messinger <zdmessinger@gmail.com>
2023-07-06 21:09:03 -0500

    added gatsby cloud

--- gatsby-config.js
+++ gatsby-config.js
@@ -15,6 +15,7 @@ module.exports = {
 		"gatsby-plugin-sharp",
 		"gatsby-transformer-sharp",
 		"gatsby-plugin-sitemap",
+		"gatsby-plugin-gatsby-cloud",
 		{
 			resolve: "gatsby-plugin-manifest",
 			options: {
