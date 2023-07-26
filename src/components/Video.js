b1af0edfb9e582d28c691ae99b94cdb89558e601
Zac Messinger <zdmessinger@gmail.com>
2023-07-26 11:50:55 -0700

    updated

--- src/components/Video.js
+++ src/components/Video.js
@@ -2,11 +2,7 @@ import * as React from "react";
 import SpaceVideo from "../assets/videos/space-video.mp4";
 
 const Video = () => {
-	return (
-		<video playsInline autoPlay loop muted preload="atuo" id="myVideo">
-			<source src={SpaceVideo} type="video/mp4" />
-		</video>
-	);
+	return <video playsInline autoPlay loop muted preload="atuo" webkit-playsinline="true" id="myVideo" src={SpaceVideo}></video>;
 };
 
 export default Video;
