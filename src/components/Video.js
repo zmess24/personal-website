db6902290fe8bcec654fbd20a6b3d683822b9507
Zac Messinger <zdmessinger@gmail.com>
2023-07-25 18:44:55 -0700

    added preload attribute on video

--- src/components/Video.js
+++ src/components/Video.js
@@ -1,12 +1,12 @@
-import * as React from 'react';
-import SpaceVideo from '../assets/videos/space-video.mp4';
+import * as React from "react";
+import SpaceVideo from "../assets/videos/space-video.mp4";
 
 const Video = () => {
-    return (
-        <video playsInline autoPlay loop muted id="myVideo">
-            <source src={SpaceVideo} type="video/mp4"/>
-        </video>
-    )
-}
+	return (
+		<video playsInline autoPlay loop muted preload="auto" id="myVideo">
+			<source src={SpaceVideo} type="video/mp4" />
+		</video>
+	);
+};
 
 export default Video;
