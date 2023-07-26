edbf672221279fc0c9b761746335bc9e1303cc66
Zac Messinger <zdmessinger@gmail.com>
2023-07-26 11:38:33 -0700

    added preload

--- src/components/Video.js
+++ src/components/Video.js
@@ -3,7 +3,7 @@ import SpaceVideo from "../assets/videos/space-video.mp4";
 
 const Video = () => {
 	return (
-		<video playsInline autoPlay loop muted id="myVideo">
+		<video playsInline autoPlay loop muted preload="atuo" id="myVideo">
 			<source src={SpaceVideo} type="video/mp4" />
 		</video>
 	);
